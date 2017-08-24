afa867be9f2d8e256a5d6bd741310e90e7f1a209
Adam Kipnis <adamkipnis@yahoo.com>
2017-08-24 12:38:05 -0700

    Added draw cards UI and integrated with MainActivity.

--- app/src/main/java/com/trustdevstudio/solitaire/MainActivity.kt
+++ app/src/main/java/com/trustdevstudio/solitaire/MainActivity.kt
@@ -2,12 +2,55 @@ package com.trustdevstudio.solitaire
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import org.jetbrains.anko.*
 
-class MainActivity : AppCompatActivity() {
+val cardBackDrawable = R.drawable.cardback_green5
+val emptyPileDrawable = R.drawable.cardback_blue1
+
+fun View.getResId(card: Card): Int {
+  val resourceName = "card${card.suit}${cardsMap[card.value]}".toLowerCase()
+  return context.resources.getIdentifier(resourceName, "drawable", context.packageName)
+}
+
+class MainActivity : AppCompatActivity(), GameView {
+  var deckView: DeckView? = null
+  var wastePileView: WastePileView? = null
+
+  override fun update(model: GameModel) {
+    deckView!!.update()
+    wastePileView!!.update()
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    GamePresenter.setGameView(this)
+    GameModel.resetGame()
+
+    // 7 card piles wide, leaving 4dp padding on each side
+    val cardWidth = (displayMetrics.widthPixels - dip(8)) / 7
+    // Card PNG is 140*190. Use cardWidth  along with ratio.
+    val cardHeight = cardWidth * (190 / 140)
+
+    verticalLayout {
+      leftPadding = dip(4)
+      rightPadding = dip(4)
+      topPadding = dip(8)
+
+      linearLayout {
+        deckView = deckView().lparams(cardWidth, cardHeight)
+        wastePileView = wastePileView().lparams(cardWidth, cardHeight)
+        view().lparams(cardWidth, 0)
+
+        for (i in 0..3)
+          imageView(imageResource = emptyPileDrawable).lparams(cardWidth, cardHeight)
+      }
+
+      linearLayout {
+
+      }
+    }
 
     // Demo Anko code. Replaces XML with object creation. Each object created passes in
     // an init function which, when called, describes the View's properties. This can include
