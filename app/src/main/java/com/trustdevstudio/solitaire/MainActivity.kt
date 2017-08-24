8787e3ebc97f731fa3675e07516faf3c47015d98
Adam Kipnis <adamkipnis@yahoo.com>
2017-08-24 11:12:20 -0700

    Converted from XML to Anko View declarations. Sample commented code for simple button/counter in MainActivity

--- app/src/main/java/com/trustdevstudio/solitaire/MainActivity.kt
+++ app/src/main/java/com/trustdevstudio/solitaire/MainActivity.kt
@@ -2,21 +2,42 @@ package com.trustdevstudio.solitaire
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        var counter = 0
+    // Demo Anko code. Replaces XML with object creation. Each object created passes in
+    // an init function which, when called, describes the View's properties. This can include
+    // style attributes, listeners, ids, etc. Also, each object created allows for calls to
+    // describe layout using lparams extension functions.
+//    var counter = 0
+//
+//    relativeLayout {
+//      val counterTextView = textView {
+//        id = 4343
+//        text = "0"
+//        textSize = 24f
+//      }
+//      button {
+//        text = "Press Me"
+//        onClick {
+//          counter++
+//          counterTextView.text = counter.toString()
+//        }
+//      }.lparams {
+//        below(counterTextView)
+//      }
+//    }
 
-        // Kotlin Android extensions allow for direct reference to widget without id lookup.
-        // Can also use lambdas in listeners.
-        counterButton.setOnClickListener( {
-            counter++
-            counterTextView.text = counter.toString()
-        })
-    }
+    // This portion now commented out to switch over to Anko
+//        // Kotlin Android extensions allow for direct reference to widget without id lookup.
+//        // Can also use lambdas in listeners.
+//        counterButton.setOnClickListener( {
+//            counter++
+//            counterTextView.text = counter.toString()
+//        })
+  }
 }
